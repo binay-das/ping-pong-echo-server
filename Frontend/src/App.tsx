c7cefc461e4d904f95aae0e64a247acb509e5fbc
Binay <binaydas1123@gmail.com>
2025-01-28 12:25:55 +0530

    Frontend init

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return <div className='bg-neutral-900 text-white w-screen h-screen'>
+    <div className='border border-white p-2'>
+      <h1 className='font-bold'>Ping-Pong Server</h1>
+    </div>
+    <div></div>
+  </div>
+}
+
+export default App
