8d170d2374c0adc2c28e79d2a2683a7d8785a51d
Binay <binaydas1123@gmail.com>
2025-01-28 16:29:03 +0530

    Instruction added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -51,17 +51,20 @@ function App() {
   return (
     <div className="w-screen h-screen flex bg-gray-100 text-gray-800">
       <div className="w-1/3 p-4 flex flex-col justify-center items-start bg-white shadow-md">
-        <h1 className="text-3xl font-bold mb-4">Ping-Pong</h1>
-        <h3 className="text-lg text-gray-500 mb-8">Echo Server</h3>
+        <div className="flex flex-col text-right">
+          <h1 className="text-3xl font-bold">Ping-Pong</h1>
+          <h3 className="text-lg text-gray-500 mb-8">Echo Server</h3>
+        </div>
         <input
           ref={inputRef}
           type="text"
           placeholder="Enter message"
-          className="w-full p-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full p-3 border border-gray-300 rounded-md focus:outline-none"
         />
+        <p className="ml-2 italic opacity-55 text-xs">Try sending <span className="font-bold">'ping'</span></p>
         <button
           onClick={sendMessage}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          className="px-4 py-2 my-4 w-full bg-blue-500 text-white rounded-md hover:bg-blue-600"
         >
           Send
         </button>
